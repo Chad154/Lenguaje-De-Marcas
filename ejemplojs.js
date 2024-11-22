be76d1713ec515223d5fcf0d28a185df8ae6f387
Chad154 <chad154154@gmail.com>
2024-11-22 12:06:38 +0100

    Carpeta de Lenguaje de marcas

---
+++ ejemplojs.js
@@ -0,0 +1,13 @@
+let numero = 42;
+let texto = "hola";
+let verdadero = true;
+let nada;
+let simbolo = Symbol("sym");
+
+//Type of saca por pantalla el tipo de dato de la variable
+
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof verdadero);
+console.log(typeof nada);
+console.log(typeof simbolo);
